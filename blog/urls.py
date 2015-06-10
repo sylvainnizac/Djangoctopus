56933caa2f5f8e7db5a1694bba945205df8c4d29
Sylvain NIZAC <sylvain.nizac@laposte.net>
2015-06-10 13:34:10 +0200

    ajout de nom court aux urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('blog.views',
-    url(r'^$', 'home'),
-    url(r'^article/(?P<id_article>\d+)$', 'view_article'),
-    url(r'^articles/(?P<year>\d{4})/(?P<month>\d{2})$', 'list_articles'),
+    url(r'^$', 'home', name="accueil"),
+    url(r'^article/(?P<id_article>\d+)$', 'view_article', name="afficher article"),
+    url(r'^articles/(?P<year>\d{4})/(?P<month>\d{2})$', 'list_articles', name="afficher liste articles"),
 )
