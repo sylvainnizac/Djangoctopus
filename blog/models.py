a6b6a8b8cdecdcd55f2b82abbcdc133e910406f1
Sylvain NIZAC <sylvain.nizac@laposte.net>
2015-06-11 13:47:07 +0200

    add first model, Article.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    """
+    This class defines the blog articles.
+    """
+    titre = models.CharField(max_length=100)
+    auteur = models.CharField(max_length=42)
+    contenu = models.TextField(null=True)
+    date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Date de parution")
+    
+    def __str__(self):
+        """ 
+        this methode is designed to be used for administration and debbuging purpose
+        """
+        return self.titre
+
