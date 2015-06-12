9ae0dc0305e8547bd9ebf5df1c1dff8d9ae78f74
sylvainnizac <sylvain.nizac@laposte.net>
2015-06-12 08:20:52 +0200

    creation elements initiaux db

--- blog/models.py
+++ blog/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 class Article(models.Model):
     """
-    This class defines the blog articles.
+    This class defines the table of blog articles.
     """
     titre = models.CharField(max_length=100)
-    auteur = models.CharField(max_length=42)
+    auteur = models.CharField(max_length=50)
     contenu = models.TextField(null=True)
     date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Date de parution")
+    categorie = models.ForeignKey('Categorie', default=0)
     
     def __str__(self):
         """ 
@@ -15,3 +16,14 @@ class Article(models.Model):
         """
         return self.titre
 
+class Categorie(models.Model):
+    """
+    This class defines the table of categories
+    """
+    nom = models.CharField(max_length=30)
+    
+    def __str__(self):
+        """ 
+        this methode is designed to be used for administration and debbuging purpose
+        """
+        return self.nom
