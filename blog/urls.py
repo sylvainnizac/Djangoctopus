af22ad7da539b2aed1622c061a94f639061c9f96
Sylvain NIZAC <sylvain.nizac@laposte.net>
2015-06-17 13:49:18 +0200

    ajout view_comments

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ urlpatterns = patterns('blog.views',
     url(r'^$', 'home', name="accueil"),
     url(r'^article/(?P<id_article>\d+)-(?P<slug>.+)$', 'view_article', name="afficher article"),
     url(r'^articles/(?P<year>\d{4})/(?P<month>\d{2})$', 'list_articles', name="afficher liste articles"),
+    url(r'^comments/(?P<id_article>\d+)-(?P<slug>.+)$', 'view_comments', name="voir les commentaires"),
     url(r'^dateactu$', 'date_actuelle', name="date actuelle"),
 )
