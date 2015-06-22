402e59a9426df0038aa49ac5532f6e48854fc0bd
Sylvain NIZAC <sylvain.nizac@laposte.net>
2015-06-22 13:35:36 +0200

    add URL to MiniURLCreate

--- urlshortner/urls.py
+++ urlshortner/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('urlshortner.views',
     url(r'^$', 'short_url_list', name="liste"),
     url(r'^new/$', 'new_short_url', name="new"),
+    url(r'^nouveau/$', MiniURLCreate.as_view(), name="url_nouveau"),
     url(r'^(?P<short>\w{6})/$', 'url_redirection', name="redirection"),
 )
