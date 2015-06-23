9de5aeae2adb68425aaf0fca4681794d146e856b
sylvainnizac <sylvain.nizac@laposte.net>
2015-06-23 19:01:42 +0200

    Revert "some tests with generic form"
    This reverts commit 1bf7d1737ce76548f44f3c2d8164b0cad3e6a5b0.

--- blog/forms.py
+++ blog/forms.py
@@ -11,7 +11,14 @@ class NewCom(forms.ModelForm):
             self.article = kwargs.pop('article')
         super(NewCom, self).__init__(*args, **kwargs)
         
-
+    def save(self, commit=True):
+        # On sauve sans faire la requête SQL (commit=False) pour
+        # pouvoir ajouter à l'instance la foreignkey
+        super(NewCom, self).save(commit=False)
+        # On ajoute à l'instance la foreignkey
+        self.instance.article = self.article
+        # On peut maintenant sauver
+        super(NewCom, self).save(commit)
         
     class Meta:
         model = Comments
--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,11 @@
 # -*- coding: utf8 -*-
 from django.conf.urls import patterns, url
-from blog.views import List_Articles, Single_Article, Leave_Comments
+from blog.views import List_Articles, Single_Article
 
 urlpatterns = patterns('blog.views',
     url(r'^$', List_Articles.as_view(), name="list_articles"),
     url(r'^categorie/(?P<id_categorie>\d+)$', List_Articles.as_view(), name="blog_categorie"),
     url(r'^article/(?P<pk>\d+)$', Single_Article.as_view(), name="lire_article"),
-    url(r'^comment/(?P<pk>\d+)-(?P<slug>.+)$', Leave_Comments.as_view(), name="commentaire"),
+    url(r'^comment/(?P<id_article>\d+)-(?P<slug>.+)$', 'leave_comments', name="commentaire"),
 )
 
