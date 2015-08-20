ede8c38e672a842574ab7b919c798c54b7859a7a
Sylvain NIZAC <sylvain.nizac@laposte.net>
2015-08-20 14:07:37 +0200

    add views for custom errors

--- blog/views.py
+++ blog/views.py
@@ -82,3 +82,15 @@ def view_comments(article_id):
     comments = Comment.objects.filter(article = article_id, commentaire_visible=True).order_by('-date')
     
     return comments
+
+def my_custom_page_not_found_view(request):
+     return render(request,'404.html')
+     
+def my_custom_error_view(request):
+     return render(request,'500.html')
+     
+def my_custom_permission_denied_view(request):
+     return render(request,'403.html')
+     
+def my_custom_bad_request_view(request):
+     return render(request,'400.html')
