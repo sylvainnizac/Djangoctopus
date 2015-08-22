1019261a80e230a25751b27a00898f0998b3ae94
sylvainnizac <sylvain.nizac@lapote.net>
2015-08-22 15:42:07 +0200

    galery creation

---
+++ galery/urls.py
@@ -0,0 +1,10 @@
+__author__ = 'Sylvain'
+# -*- coding: utf8 -*-
+from django.conf.urls import patterns, url
+from blog.views import List_Articles, Single_Article
+
+urlpatterns = patterns('blog.views',
+    url(r'^$', , name="carousel"),
+)
+
+
