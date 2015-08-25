cccced563a290133ad71c2d2d48d1c9a89d7209b
sylvainnizac <sylvain.nizac@laposte.net>
2015-08-26 00:40:32 +0200

    remove 2 error handlers

--- st_pages/views.py
+++ st_pages/views.py
@@ -2,24 +2,21 @@ from django.shortcuts import render, render_to_response
 from django.template import RequestContext
 
 # Create your views here.
-
+"""
 def my_custom_page_not_found_view(request):
     response = render_to_response('errors/404.html', {},
                                   context_instance=RequestContext(request))
     response.status_code = 404
     return response
 
-def my_custom_error_view(request):
-    response = render_to_response('errors/500.html', {},
-                                  context_instance=RequestContext(request))
-    response.status_code = 500
-    return response
-
 def my_custom_permission_denied_view(request):
      return render(request,'errors/403.html')
-
-#def my_custom_bad_request_view(request):
- #    return render(request,'errors/400.html')
+"""
+def my_custom_bad_request_view(request):
+     return render(request,'errors/400.html')
+     
+def my_custom_error_view(request):
+    return render(request,'errors/500.html')
 
 def CV(request):
      return render(request,'st_pages/CV.html')
