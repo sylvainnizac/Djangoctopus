5bb63d3102f50afbc2ca8e3c99a632c99d95d0e0
Sylvain NIZAC <sylvain.nizac@laposte.net>
2015-09-16 13:57:53 +0200

    ajout urls pics_list

--- galery/urls.py
+++ galery/urls.py
@@ -5,5 +5,8 @@ from galery.views import Main_carousel, Main_galery
 urlpatterns = patterns('galery.views',
     url(r'^$', Main_carousel.as_view(), name="carousel"),
     url(r'^photos/$', Main_galery.as_view(), name="galery"),
-    #url(r'^photos/json$', 'pics_list', name="photos_all"),
+    url(r'^photos/json$', 'pics_list', name="photos_all"),
+    url(r'^photos/(?P<faction>\d+)/json$', 'pics_list', name="photo_faction"),
+    url(r'^photos/(?P<secto>\d+)/json$', 'pics_list', name="photo_sectorielle"),
+    url(r'^photos/(?P<fig>\d+)/json$', 'pics_list', name="photo_figurine"),
 )
