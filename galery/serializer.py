16d13d58af1f07f9533031fe28b8e6adb230b814
Sylvain NIZAC <sylvain.nizac@laposte.net>
2015-09-16 13:33:15 +0200

    creation PhotoSerializer

--- galery/serializer.py
+++ galery/serializer.py
@@ -1,1 +1,13 @@
-__author__ = 'sylvain'
+# -*- coding: utf8 -*-
+from rest_framework import serializers
+from galery.models import Photo
+
+
+class PhotoSerializer(serializers.ModelSerializer):
+    """
+    standard serialization class, like for forms
+    create() and update() methods already impemented
+    """
+    class Meta:
+        model = Photo
+        fields =('pic_path', )
